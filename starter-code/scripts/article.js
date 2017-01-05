3481b1e2c19439c52ce23af38a985fc182c98ecc
caylazabel <caylazabel@gmail.com>
2017-01-05 13:40:26 -0800

    added script tag for handlebars and added template

--- starter-code/scripts/article.js
+++ starter-code/scripts/article.js
@@ -3,13 +3,10 @@
 var articles = [];
 
 function Article (opts) {
-  this.author = opts.author;
-  this.authorUrl = opts.authorUrl;
-  this.title = opts.title;
-  this.category = opts.category;
-  this.body = opts.body;
-  this.publishedOn = opts.publishedOn;
-}
+  for(key in opts){
+    this[key] = opts[key];
+  }
+};
 
 Article.prototype.toHtml = function() {
   // TODO: Use handlebars to render your articles.
